d6165dafaa8b4e66c1b8751342049ef906231243
Pritam Maharjan <primgdev@gmail.com>
2024-05-11 12:20:42 +1000

    feature/3/homepage

--- src/App.js
+++ src/App.js
@@ -9,9 +9,16 @@ function App() {
 
 
       <Navigation />
-      
+      <div className='title'>
+        <h1 className='wrapper'>
+          <span > Hello. I'm Pritam</span><br></br>
+          <span> A Web Developer</span>
+        </h1>
+        <p className='description'> I am passionate about creating experiences that are easy to use, accessible, and that meet the user's needs.</p>
+      </div>
 
-    
+
+
     </div>
   );
 }

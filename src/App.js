3b981a82ed9f8395e8f1c4f2ed2ae5af9e211459
Pritam Maharjan <primgdev@gmail.com>
2024-05-14 19:57:40 +1000

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import DocumentScannerOutlinedIcon from '@mui/icons-material/DocumentScannerOutlined';
 import { useState, useEffect } from 'react';
 import Loader from './components/loader';
+import Scroll from './components/scroll';
 
 
 function App() {
@@ -15,26 +16,26 @@ function App() {
   const [audio] = useState(new Audio(url));
   const [playing, setPlaying] = useState(false);
   const [loading, setLoading] = useState(false);
-  
+
 
 
   useEffect(() => {
     // Simulate data fetching or some initialization
     const fetchData = async () => {
-        setLoading(true);
-        try {
-            // Simulate a delay for fetching data
-            await new Promise(resolve => setTimeout(resolve, 500));
-            
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        } finally {
-            setLoading(false);
-        }
+      setLoading(true);
+      try {
+        // Simulate a delay for fetching data
+        await new Promise(resolve => setTimeout(resolve, 500));
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchData();
-}, []);
+  }, []);
 
 
   useEffect(() => {
@@ -60,23 +61,32 @@ function App() {
 
   return (
     <div className="App">
+      <section id="home">
+
+        <div className='title'>
+          <Navigation />
+          <h1 className='wrapper'>
+            <span > Hello. I'm Pritam</span><br></br>
+            <span> A Web Developer</span>
+          </h1>
+          <p className='description'> I am passionate about creating experiences that are easy to use, accessible, and that meet the user's needs.</p>
+          <Button onClick={handleDownload} style={{ background: 'linear-gradient(36deg, rgba(153,245,198,0.8183648459383753) 0%, rgba(247,173,246,0.6895133053221288) 99%)', border: '1px solid #bbbbbb', color: 'black', fontSize: '13px' }} variant="outlined" startIcon={<DocumentScannerOutlinedIcon />} >
+            Resume
+          </Button>
+          {loading ? <Loader /> : <h1>{ }</h1>}
+        </div>
+      </section>
+
+      <section id="about"><div></div>
+        <p style={{ color: 'white' }}>iahsdfaofhaoifh oafafn alfshao fsh aisdh oah </p>
 
 
+      </section>
 
-      <Navigation />
-      <div className='title'>
-        <h1 className='wrapper'>
-          <span > Hello. I'm Pritam</span><br></br>
-          <span> A Web Developer</span>
-        </h1>
-        <p className='description'> I am passionate about creating experiences that are easy to use, accessible, and that meet the user's needs.</p>
-        <Button onClick={handleDownload} style={{ background: 'linear-gradient(36deg, rgba(153,245,198,0.8183648459383753) 0%, rgba(247,173,246,0.6895133053221288) 99%)', border: '1px solid #bbbbbb', color: 'black', fontSize: '13px' }} variant="outlined" startIcon={<DocumentScannerOutlinedIcon />} >
-          Resume
-        </Button>
-        {loading ? <Loader /> : <h1>{}</h1>}
-      </div>
-      </div>
-    
+      <section id="work">Work</section>
+
+    </div>
+
   );
 }
 
